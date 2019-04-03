3f78d66e0a119c84fc0f002787aabd712d26464d
Yashar <yashar.p@780.ir>
2019-04-04 01:22:20 +0430

    Adjusted for better preview

--- app/src/main/java/com/yashoid/sequencelayout/sample/ResizeableParent.java
+++ app/src/main/java/com/yashoid/sequencelayout/sample/ResizeableParent.java
@@ -11,6 +11,9 @@ public class ResizeableParent extends SequenceLayout {
     private int mMinimumWidth = -1;
     private int mMinimumHeight = -1;
 
+    private float mTouchX = 0;
+    private float mTouchY;
+
     public ResizeableParent(Context context) {
         super(context);
     }
@@ -32,8 +35,28 @@ public class ResizeableParent extends SequenceLayout {
             mMinimumHeight = getMeasuredHeight();
         }
 
-        int width = Math.max(mMinimumWidth, (int) event.getX());
-        int height = Math.max(mMinimumHeight, (int) event.getY());
+        if (event.getAction() != MotionEvent.ACTION_DOWN && event.getAction() != MotionEvent.ACTION_MOVE) {
+            mTouchX = 0;
+            mTouchY = 0;
+
+            return true;
+        }
+
+        if (mTouchX == 0) {
+            mTouchX = event.getX();
+            mTouchY = event.getY();
+
+            return true;
+        }
+
+        int dx = (int) (event.getX() - mTouchX);
+        int dy = (int) (event.getY() - mTouchY);
+
+        mTouchX = event.getX();
+        mTouchY = event.getY();
+
+        int width = Math.max(mMinimumWidth, getWidth() + dx);
+        int height = Math.max(mMinimumHeight, getHeight() + dy);
 
         LayoutParams params = getLayoutParams();
 
